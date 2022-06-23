cd055892b51a182066b17762791f328971c0013b
Brian Acebo <brianacebo@Brians-MBP.attlocal.net>
2022-06-23 14:38:16 -0400

    Fixed screen x scroll issue

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,7 +3,7 @@
   import MainNav from "$lib/components/mainNav/MainNav.svelte";
 </script>
 
-<div class="overflow-x-hidden w-screen">
+<div class="overflow-x-hidden w-screen max-w-[100vw]">
 <MainNav />
 
 <slot></slot>
